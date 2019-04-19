db5f0b983a17c198c2108ae3be26376ca158a9c6
Annie Huang <annie.hn@gmail.com>
2019-04-19 16:06:02 +1000

    Add SuperLibModule into app.module.ts

--- ng-packagr-demo1/src/app/app.module.ts
+++ ng-packagr-demo1/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SuperLibModule } from 'super-lib';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SuperLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
